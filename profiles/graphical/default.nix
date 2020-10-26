32dc9353f38117f9bd9a17925f582f56529a8639
Louis <louis@bettens.info>
2020-10-26 22:01:31 +0100

    Add gnome3 and autologin

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -3,7 +3,23 @@
 {
   environment.systemPackages = with pkgs; [
     brave
+    gimp
+    gnome3.dconf-editor
+    gnome3.gnome-tweaks
   ];
 
   programs.browserpass.enable = true;
+
+  programs.geary.enable = false;  # doesn't build for some reason
+  environment.gnome3.excludePackages = with pkgs; [
+    gnome3.epiphany  # I don't use
+  ];
+
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  services.xserver.displayManager = {
+    defaultSession = "gnome";
+    lightdm.autoLogin.enable = true;
+    lightdm.autoLogin.user = "louis";
+  };
 }
