b942dce0a726189a7f9629934af7b18104791ac7
Louis Bettens <louis@bettens.info>
2020-10-28 14:53:32 +0100

    import hadron settings

---
+++ hosts/hadron.nix
@@ -0,0 +1,46 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      hadron/hardware-configuration.nix
+      ../profiles/graphical
+      ../users/louis
+    ];
+
+  # GRUB
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.grub.efiInstallAsRemovable = true;
+
+  boot.initrd.kernelModules = [ "dm-raid" ];
+
+  networking.hostName = "hadron"; # Define your hostname.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = true;
+
+  services.earlyoom.enable = true;
+
+  users.mutableUsers = false;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+
+  fileSystems."/home/nfs" = {
+  device = "machaut.ici:/home";
+  fsType = "nfs";
+  };
+}
