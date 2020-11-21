c0051bdae37a90738937d6c834e7400e6a8410d3
Louis Bettens <louis@bettens.info>
2020-11-21 23:34:08 +0100

    Update hadronsson hostnames

--- modules/trusted-machines/default.nix
+++ modules/trusted-machines/default.nix
@@ -7,7 +7,7 @@
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/2mQVukHogdz14D0PGdjlew5YQTOFKZV7G08EqU5Vo";
     };
     hadronsson = {
-      hostNames = [ "hadronsson" "hadronsson-fil" ];
+      hostNames = [ "hadronsson" "hadronsson-wl" ];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHdhemK8JsrQLCZgKwF/9XYoeHKxFZP9KwOLP76cuuFJ";
     };
   };
