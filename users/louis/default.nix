423e86d560bff998813ad894059dc81a8e64ffa1
Louis Bettens <louis@bettens.info>
2020-11-21 11:33:44 +0100

    Add backup hashed password (stored in pass)

--- users/louis/default.nix
+++ users/louis/default.nix
@@ -23,5 +23,7 @@
     openssh.authorizedKeys.keyFiles = [
       ./id_gpgcard.pub
     ];
+
+    hashedPassword = "$6$nZVlYP4/z34a$abhkQIZS6udFg6H/w2q4P37SsWDhaL0tEeDMfWqO2BbBs4NOI0lbIDXjhvs1l058YSSYOAyecRiaAByNywDO91";
   };
 }
