40890ffefedb77603af9c72b49bc4af3db93a11e
Louis Bettens <louis@bettens.info>
2020-12-08 11:59:32 +0100

    binary cache doesn't work in installer

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -162,17 +162,6 @@ in
       fallback = true
     '';
 
-    sshServe = {
-      enable = true;
-      protocol = "ssh-ng";
-      keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJW+XKP9JADl1RnL9BcJdKiSvDFnggRcY7gqP3vp5Gsb root@hadronsson"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINp3VzaG3ki6kz3/dKmUwMeel4fGWhwkpFD5NW6p5AEG root@niximg"
-      ];
-    };
-
-    signStorePaths = true;
-
     binaryCaches = lib.mkAfter (lib.optional (config.networking.hostName != "hadron" && !config.special.roaming) "ssh-ng://nix-ssh@hadron");
   };
 
--- profiles/utils/default.nix
+++ profiles/utils/default.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 {
+  nix.signStorePaths = true;
+  nix.sshServe = {
+    enable = true;
+    protocol = "ssh-ng";
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJW+XKP9JADl1RnL9BcJdKiSvDFnggRcY7gqP3vp5Gsb root@hadronsson"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINp3VzaG3ki6kz3/dKmUwMeel4fGWhwkpFD5NW6p5AEG root@niximg"
+    ];
+  };
+
   programs.java = {
     enable = true;
     package = pkgs.jdk11;
