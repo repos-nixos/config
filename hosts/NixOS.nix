ec19034aee348a4dbeae50d897b6e085eb04234a
Louis Bettens <louis@bettens.info>
2020-12-19 16:23:04 +0100

    Enable raid support on installer

--- hosts/NixOS.nix
+++ hosts/NixOS.nix
@@ -5,6 +5,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.initrd.kernelModules = [ "dm-raid" ];
+
   programs.gnupg.agent.pinentryFlavor = "curses";
 
   networking.networkmanager.enable = true;
