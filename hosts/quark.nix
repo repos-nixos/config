f8356fe24cf4b9075d0f3b02df314fecbbab1178
Louis Bettens <louis@bettens.info>
2020-12-25 18:18:44 +0100

    Fix quark

--- hosts/quark.nix
+++ hosts/quark.nix
@@ -1,8 +1,8 @@
-{ config, modulesPath, ... }: {
+{ config, pkgs, ... }: {
   imports = [
     ../users/louis
-    ../profiles/graphical
     ../profiles/utils
+    quark/hardware-configuration.nix
   ];
 
   nixpkgs.system = "aarch64-linux";
@@ -13,4 +13,12 @@
     version = 4;
   };
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
