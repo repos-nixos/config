137c2780c463f7cd90c1be8d7bf1d7681bc66699
Louis Bettens <louis@bettens.info>
2020-12-26 12:06:10 +0100

    quark: set up swapfile and gnome

--- hosts/quark.nix
+++ hosts/quark.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   imports = [
     ../users/louis
+    ../profiles/graphical
     ../profiles/utils
     quark/hardware-configuration.nix
   ];
@@ -13,6 +14,30 @@
     version = 4;
   };
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
+
+  swapDevices = [ {
+    device = "/var/swapfile";
+    size = 8192;
+    randomEncryption.enable = true;
+  } ];
+
+  # Required for the Wireless firmware
+  hardware.enableRedistributableFirmware = true;
+
+  hardware.opengl = {
+    enable = true;
+    setLdLibraryPath = true;
+    package = pkgs.mesa_drivers;
+  };
+  hardware.deviceTree = {
+    overlays = [ "${pkgs.device-tree_rpi.overlays}/vc4-fkms-v3d.dtbo" ];
+  };
+
+  services.xserver.videoDrivers = [ "modesetting" ];
+  boot.loader.raspberryPi.firmwareConfig = ''
+    gpu_mem=192
+    disable_overscan=1
+  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
