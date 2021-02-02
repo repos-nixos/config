b1661094e51172a91bb218c59098e067315a0adf
Louis Bettens <louis@bettens.info>
2021-02-02 15:54:27 +0100

    user/xcape: take on lshift

--- users/profiles/xcape/default.nix
+++ users/profiles/xcape/default.nix
@@ -5,6 +5,9 @@
       "Alt_L" = "Escape";
       "Control_R" = "Tab";
       "Shift_R" = "Shift_R|Tab";
+
+      # Compensate the lack of a ≷ on ANSI boards
+      "Shift_L" = "less";
     };
   };
 }
