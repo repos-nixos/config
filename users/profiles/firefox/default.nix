1bf7ece7d2ecc0d88132fecb0877b6b9d6412259
Louis Bettens <louis@bettens.info>
2021-02-22 18:25:57 +0100

    firefox: install UBlock Origin

--- users/profiles/firefox/default.nix
+++ users/profiles/firefox/default.nix
@@ -4,6 +4,10 @@
   programs.firefox = {
     enable = true;
 
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+    ];
+
     profiles.default = {
       id = 0;
       settings = {
