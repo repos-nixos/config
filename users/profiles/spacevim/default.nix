dde471dc7299a2721939067e56ae8b611be1f3df
Louis Bettens <louis@bettens.info>
2021-02-22 09:14:35 +0100

    spacevim: add Python layer

--- users/profiles/spacevim/default.nix
+++ users/profiles/spacevim/default.nix
@@ -4,7 +4,12 @@
 let spacevim = pkgs.callPackage ./pkg { };
 in
 {
-  home.packages = [ spacevim ];
+  home.packages = [
+    spacevim
+  ] ++ (with pkgs.python3Packages; [
+    pylint
+    yapf
+  ]);
 
 
   home.file.".SpaceVim.d/init.toml" = {
