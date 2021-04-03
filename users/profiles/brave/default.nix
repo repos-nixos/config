9a496fc15e982c00daa47add76347946a41b2ae3
Louis Bettens <louis@bettens.info>
2021-04-03 12:16:13 +0200

    brave: add add-ons

--- users/profiles/brave/default.nix
+++ users/profiles/brave/default.nix
@@ -5,7 +5,11 @@
     enable = pkgs.system == "x86_64-linux";
 
     extensions = [
-      "hkgfoiooedgoejojocmhlaklaeopbecg"  # Picture-in-picture by Google
+      "hkgfoiooedgoejojocmhlaklaeopbecg" # Picture-in-picture by Google
+      "pbcodcjpfjdpcineamnnmbkkmkdpajjg" # Link to text fragment by Google
+      "eekailopagacbcdloonjhbiecobagjci" # Go back with Backspace by Google
+      "fllaojicojecljbmefodhfapmkghcbnh" # Google Analytics opt-out
+      "gbiekjoijknlhijdjbaadobpkdhmoebb" # Google Adds opt-out
     ];
   };
 }
