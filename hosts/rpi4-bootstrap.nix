65618f62bf53c856e55edf9f5c4c025b64cbbe8b
Louis Bettens <louis@bettens.info>
2021-05-10 09:29:03 +0200

    unbreak rpi4-bootstrap

--- hosts/rpi4-bootstrap.nix
+++ hosts/rpi4-bootstrap.nix
@@ -1,39 +1,19 @@
-# based on https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/cd-dvd/sd-image-raspberrypi4.nix
-
-{ config, lib, modulesPath, pkgs, ... }: {
+{ config, lib, modulesPath, pkgs, suites, ... }: {
   imports = suites.base ++ [
-    ../users/root
-    ../users/nixos
-    "${modulesPath}/installer/cd-dvd/sd-image.nix"
+    "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
   ];
-
   nixpkgs.system = "aarch64-linux";
 
-  boot.loader.grub.enable = false;
-  boot.loader.raspberryPi = {
-    enable = true;
-    version = 4;
-  };
-  boot.kernelPackages = pkgs.linuxPackages_rpi4;
-  boot.consoleLogLevel = lib.mkDefault 7;
-
-  sdImage = {
-    firmwareSize = 128;
-    firmwarePartitionName = "NIXOS_BOOT";
-    # This is a hack to avoid replicating config.txt from boot.loader.raspberryPi
-    populateFirmwareCommands =
-      "${config.system.build.installBootLoader} ${config.system.build.toplevel} -d ./firmware";
-    # As the boot process is done entirely in the firmware partition.
-    populateRootCommands = "";
-  };
-
-  fileSystems."/boot/firmware" = {
-    # This effectively "renames" the attrsOf entry set in sd-image.nix
-    mountPoint = "/boot";
-    neededForBoot = true;
-  };
-
-  services.mingetty.autologinUser = "louis";
-
   programs.gnupg.agent.pinentryFlavor = "curses";
+
+  sdImage.populateRootCommands = ''
+    mkdir -m 700 -p ./files/root/
+    cp ${../secrets/id_niximg} ./files/root/id_niximg
+    chmod 600 ./files/root/id_niximg
+  '';
+
+  programs.ssh.extraConfig = ''
+    IdentityFile /root/id_niximg
+  '';
+
 }
