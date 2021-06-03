d7fff2cb22bb8f9daa0806d75588955058312f19
Louis Bettens <louis@bettens.info>
2021-06-03 12:36:47 +0200

    remove niximg

--- hosts/rpi4-bootstrap.nix
+++ hosts/rpi4-bootstrap.nix
@@ -4,15 +4,4 @@
   ];
 
   programs.gnupg.agent.pinentryFlavor = "curses";
-
-  sdImage.populateRootCommands = ''
-    mkdir -m 700 -p ./files/root/
-    cp ${../secrets/id_niximg} ./files/root/id_niximg
-    chmod 600 ./files/root/id_niximg
-  '';
-
-  programs.ssh.extraConfig = ''
-    IdentityFile /root/id_niximg
-  '';
-
 }
