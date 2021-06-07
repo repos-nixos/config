1bec347f40b005c91dd9689b8e7d809f8a7eb56c
Louis Bettens <louis@bettens.info>
2021-06-07 11:30:35 +0200

    brave/extensions: uninstall backspace

--- users/profiles/brave/default.nix
+++ users/profiles/brave/default.nix
@@ -7,7 +7,6 @@
     extensions = [
       "hkgfoiooedgoejojocmhlaklaeopbecg" # Picture-in-picture by Google
       "pbcodcjpfjdpcineamnnmbkkmkdpajjg" # Link to text fragment by Google
-      "eekailopagacbcdloonjhbiecobagjci" # Go back with Backspace by Google
       "fllaojicojecljbmefodhfapmkghcbnh" # Google Analytics opt-out
       "gbiekjoijknlhijdjbaadobpkdhmoebb" # Google Adds opt-out
     ];
