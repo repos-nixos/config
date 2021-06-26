859952eea4fe10813a1057cf722a1725603fc008
Louis Bettens <louis@bettens.info>
2021-06-27 01:09:28 +0200

    fix gnome extensions

--- users/profiles/gnome/shell.nix
+++ users/profiles/gnome/shell.nix
@@ -3,10 +3,10 @@
 # tweaks for my Gnome Desktop
 
 let extensions = with pkgs.gnomeExtensions; [
-  gnome-shell-extension-gtk-title-bar
-  gnome-shell-extension-hide-activities-button
+  gtk-title-bar
+  hide-activities-button
   hot-edge
-  gnome-shell-extension-removable-drive-menu
+  removable-drive-menu
 ];
 
 in
